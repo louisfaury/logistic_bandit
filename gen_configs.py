599bf5a3e5c8ac1ffdcae7b62750e2ccec8e7228
Louis Faury <l.faury@criteo.com>
2021-08-31 16:13:44 +0200

    add param norm

--- gen_configs.py
+++ gen_configs.py
@@ -14,12 +14,18 @@ for file in os.listdir(config_dir):
 repeat = 100
 horizon = 1000
 dims = np.array([2, 5, 10, 20])
+param_norm = np.array([2, 3, 4, 5])
 algos = ['GLM-UCB', 'LogUCB1', 'OFULog-r', 'OL2M', 'GLOC', 'ECOLog']
 
 # create configs
 for d in dims:
-    for algo in algos:
-        config_path = os.path.join(config_dir, 'h{}d{}a{}.json'.format(horizon, d, algo))
-        config_dict = {"repeat": int(repeat), "horizon": int(np.ceil(d)*horizon), "dimension": int(d), "algo": algo}
-        with open(config_path, 'w') as f:
-            json.dump(config_dict, f)
+    for pn in param_norm:
+        for algo in algos:
+            theta_star = pn / np.sqrt(d) * np.ones([d])
+            pn_ub = pn + 1
+            config_path = os.path.join(config_dir, 'h{}d{}a{}n{}.json'.format(horizon, d, algo, pn))
+            config_dict = {"repeat": int(repeat), "horizon": int(np.ceil(d)*horizon), "dimension": int(d), "algo": algo,
+                           "theta_star": theta_star.tolist(), "param_norm_ub": int(pn_ub)}
+
+            with open(config_path, 'w') as f:
+                json.dump(config_dict, f)
