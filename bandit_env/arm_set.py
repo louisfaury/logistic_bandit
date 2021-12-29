e0006af35cc68768d4330ad264ad972a6cb6bb5f
Louis Faury <l.faury@criteo.com>
2021-12-29 16:16:47 +0100

    clean arm-set, add AdmissibleArmSet class

--- bandit_env/arm_set.py
+++ bandit_env/arm_set.py
@@ -1,4 +1,12 @@
 import numpy as np
+from enum import Enum
+
+
+class AdmissibleArmSet(Enum):
+    fxd = 'fixed_discrete'
+    tvd = 'tv_discrete'
+    ball = 'ball'
+
 
 """
 Class for the arm set. 
@@ -15,14 +23,9 @@ arm_norm_ub : float
     upper-bound on the ell-two norm of the arms
 """
 
-admissible_arm_set_type = ['fixed_discrete', 'tv_discrete', 'ball']
-
 
 class ArmSet(object):
     def __init__(self, arm_set_type, dim, length, arm_norm_ub):
-        if arm_set_type not in admissible_arm_set_type:
-            raise ValueError('{} set type is not admissible'.format(arm_set_type))
-
         self.type = arm_set_type
         self.dim = dim
         self.length = length
@@ -31,34 +34,29 @@ class ArmSet(object):
 
     def generate_arm_list(self):
         """
-        Compute and stores the arm list
-        :return: None
+        Compute and stores the arm list.
         """
-        if not self.type == 'ball':
+        if not self.type == AdmissibleArmSet.ball:
             u = np.random.normal(0, 1, (self.length, self.dim))
             norm = np.linalg.norm(u, axis=1)[:, None]
             r = np.random.uniform(0, 1, (self.length,1)) ** (1.0 / self.dim)
             self.arm_list = r * u / norm
 
-    def argmax(self, max_fun):
+    def argmax(self, fun):
         """
-        Find the arm which maximizes max_fun
-        /!\ : only valid for finite arm set
-        :param max_fun: function to maximize
-        :return:
+        Find the arm which maximizes fun (only valid for finite arm sets).
+        :param fun: function to maximize
         """
-        if self.type == 'ball':
+        if self.type == AdmissibleArmSet.ball:
             raise ValueError('argmax function is only compatible with finite arm sets')
-
-        arm_and_values = list(zip(self.arm_list, [max_fun(a) for a in self.arm_list]))
+        arm_and_values = list(zip(self.arm_list, [fun(a) for a in self.arm_list]))
         return max(arm_and_values, key=lambda x: x[1])[0]
 
     def random(self):
         """
-        Draw a random arm from arm_set
-        :return: np.array(dim), random arm
+        Draw and returns a random arm from arm_set.
         """
-        if self.type == 'ball':
+        if self.type == AdmissibleArmSet.ball:
             u = np.random.normal(0, 1, self.dim)
             norm = np.sqrt(np.sum(u ** 2))
             r = np.random.uniform(0, self.arm_norm_ub) ** (1.0 / self.dim)
